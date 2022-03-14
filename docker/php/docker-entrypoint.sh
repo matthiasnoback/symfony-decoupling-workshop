458182b72a0b4e0ac94cef13ce9c3bb73313cb05
Matthias Noback <matthiasnoback@gmail.com>
2022-03-14 11:21:42 +0100

    Remove prod config

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -7,32 +7,10 @@ if [ "${1#-}" != "$1" ]; then
 fi
 
 if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
-	PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-production"
-	if [ "$APP_ENV" != 'prod' ]; then
-		PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-development"
-	fi
-	ln -sf "$PHP_INI_RECOMMENDED" "$PHP_INI_DIR/php.ini"
-
 	mkdir -p var/cache var/log
 
-	# The first time volumes are mounted, the project needs to be recreated
-	if [ ! -f composer.json ]; then
-		CREATION=1
-		composer create-project "$SKELETON $SYMFONY_VERSION" tmp --stability="$STABILITY" --prefer-dist --no-progress --no-interaction --no-install
-
-		cd tmp
-		composer require "php:>=$PHP_VERSION"
-		composer config --json extra.symfony.docker 'true'
-		cp -Rp . ..
-		cd -
-
-		rm -Rf tmp/
-	fi
-
-	if [ "$APP_ENV" != 'prod' ]; then
-		rm -f .env.local.php
-		composer install --prefer-dist --no-progress --no-interaction
-	fi
+  rm -f .env.local.php
+  composer install --prefer-dist --no-progress --no-interaction
 
 	if grep -q ^DATABASE_URL= .env; then
 		if [ "$CREATION" = "1" ]; then
